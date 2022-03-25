1e98a6f483888e97c3b6a5d6dabcc92233ccca19
fthgncl <gencal.fatih61@gmail.com>
2022-03-25 11:05:50 +0300

    Ön tanımlamalar aktarıldı

--- blynk_control.ino
+++ blynk_control.ino
@@ -1,12 +1,3 @@
-/* Blynk includes */
-#define BLYNK_TEMPLATE_ID "TMPLixMwkJME"
-#define BLYNK_DEVICE_NAME "Led Kontrol Cihazım"
-#define BLYNK_AUTH_TOKEN "_cSSGCiFYwuC710XfSsZct2krnvqUR99"
-#define BLYNK_PRINT Serial
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-/***********************/
-
 char ssid[] = "TRC";
 char pass[] = "ist34ank06";
 
