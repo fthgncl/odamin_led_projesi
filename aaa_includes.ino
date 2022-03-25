24cfb7bcf5887a962ec40cffae3cbe47438e355e
fthgncl <gencal.fatih61@gmail.com>
2022-03-25 11:05:27 +0300

    ön tanımlamalar için yeni dosya oluşturuldu

---
+++ aaa_includes.ino
@@ -0,0 +1,16 @@
+/* Blynk includes */
+#define BLYNK_TEMPLATE_ID "TMPLixMwkJME"
+#define BLYNK_DEVICE_NAME "Led Kontrol Cihazım"
+#define BLYNK_AUTH_TOKEN "_cSSGCiFYwuC710XfSsZct2krnvqUR99"
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+/***********************/
+
+/* NTP Client includes */
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+WiFiUDP ntpUDP;
+const byte GMT = 3;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600*GMT);
+/***********************/
