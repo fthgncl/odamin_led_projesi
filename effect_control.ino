5f364ee13a8c73bb8792fbf8e51fd878305ad693
fthgncl <gencal.fatih61@gmail.com>
2022-03-26 18:08:10 +0300

    optimize edildi

--- effect_control.ino
+++ effect_control.ino
@@ -3,13 +3,13 @@
 Effect allEffects[EFFECT_COUNTS];
 bool firtTimeEffect[EFFECT_COUNTS];
 
-Effect EF_reel00;
-
 void effect_control_setup() {
 
-  EF_reel00.create(continuousUse);
-  allEffects[0] = EF_reel00;
-  EF_reel00.addWorkTime(12, 06, 12, 06);
+  allEffects[0].create(continuousUse);  /// DemoReel
+  allEffects[0].addWorkTime(17, 52, 17, 52);
+
+  allEffects[1].create(singleUse);  /// Avize Kemeri
+  allEffects[1].addWorkTime(15, 00, 20, 00);
 
 }
 void effect_control_loop() {
@@ -17,29 +17,48 @@ void effect_control_loop() {
   timeClient.update();
 
   for ( byte i = 0 ; i < effectCount; i++ ) {
-    bool runTime = allEffects[i].isTime();
-    if ( runTime ) {
-      if ( firstTimeEffectControl(i, runTime)) {
-        Serial.print(i);
-        Serial.println(" -> ilk defa çalıştı.");
+    if (allEffects[i].enable) {
+      bool runTime = allEffects[i].isTime();
+      getEffectOnOFF(i, runTime);
+      if ( runTime && allEffects[i].useType == continuousUse ) {
+        runEffect(i, continuousUse);
       }
-      else
-        runEffect(i);
     }
   }
-  delay(2000);
 }
-void runEffect(byte num) {
-  switch (allEffects[num].id) {
-        case EF_reel00.id: ReelEfect();
-          break;
-
-bool firstTimeEffectControl(byte i, bool runTime) {
+void runEffect(byte num, bool useType) {
+  if ( useType == singleUse ) {       // Single Use
+    switch ( allEffects[num].id ) {
+      case 1 : avizeKemeri();
+        break;
+    }
+  }
+  else                              // Continuous Use
+  {
+    switch ( allEffects[num].id ) {
+      case 0 : ReelEfect();
+        break;
+    }
+  }
+}
+void getEffectOnOFF(byte i, bool runTime) {
 
   if ( firtTimeEffect[i] != runTime ) {
     firtTimeEffect[i] = runTime;
-    return true;
-  }
 
-  return false;
+    if ( firtTimeEffect[i] )
+      effectStartProtocol(i);
+    else
+      effectStopProtocol(i);
+
+  }
+}
+void effectStartProtocol(byte num) {
+  runEffect(num, allEffects[num].useType);
+}
+void effectStopProtocol(byte num) {
+  clearLeds();
+  for ( byte i = 0 ; i < EFFECT_COUNTS ; i++ )
+    if ( allEffects[i].id != -1 && allEffects[i].isTime()&& allEffects[i].useType == singleUse )
+      runEffect(i, singleUse);
 }
