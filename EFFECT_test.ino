c1b2cf355bcaa5c8c1d16b620e54f02194a499ca
fthgncl <gencal.fatih61@gmail.com>
2022-03-26 18:07:20 +0300

    efekt eklendi

---
+++ EFFECT_test.ino
@@ -0,0 +1,11 @@
+
+void avizeKemeri() {
+  byte color[3];
+  color[0] = random(50);
+  color[1] = random(50);
+  color[2] = random(50);
+  for ( int i = 289 ; i > 233 ; i --){
+    leds[i].setRGB(color[0],color[1],color[2]);
+  }
+  FastLED.show();  
+}
