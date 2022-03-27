e598706a0f6e0dfe524cee93fed4eeece318a384
fthgncl <gencal.fatih61@gmail.com>
2022-03-27 11:54:52 +0300

    blynk kontrolü sağlandı

--- blynk_control.ino
+++ blynk_control.ino
@@ -1,9 +1,76 @@
 char ssid[] = "TRC";
 char pass[] = "ist34ank06";
 
+#define PIN_RunByTime = 0
+byte manualBlynkRGB[3];
+
 void blynk_setup() {
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
+
 void blynk_loop() {
   Blynk.run();
+}
+
+void blynkUpdateDashBoard() {
+  Blynk.virtualWrite(V0, runByTime);
+  Blynk.virtualWrite(V1, getEffectManualWorkStatus(0));
+  Blynk.virtualWrite(V2, getEffectManualWorkStatus(1));
+  Blynk.virtualWrite(V3, getEffectManualWorkStatus(2));
+  Blynk.virtualWrite(V8, getEffectManualWorkStatus(3));
+}
+
+BLYNK_CONNECTED() {
+  Blynk.syncAll();
+  blynkUpdateDashBoard();
+}
+
+BLYNK_WRITE(V0)  {  // Zamana Göre İşleyiş
+  runByTime = param.asInt();
+  blynkUpdateDashBoard();
+}
+
+BLYNK_WRITE(V1)  {  // Efekt 1
+  changeEffectManualWorkStatus(0, param.asInt());
+}
+
+BLYNK_WRITE(V2)  {  // Efekt 2
+  changeEffectManualWorkStatus(1, param.asInt());
+}
+
+BLYNK_WRITE(V3)  {  // Efekt 3
+  changeEffectManualWorkStatus(2, param.asInt());
+}
+
+BLYNK_WRITE(V8)  {  // Efekt 4
+  changeEffectManualWorkStatus(3, param.asInt());
+}
+
+BLYNK_WRITE(V4)  {
+  Serial.println("zxxxxxxxxxxxxxxxxxxxxxx");
+}
+BLYNK_WRITE(V5)  {
+  manualBlynkRGB[0] = param.asInt();
+  setManualRGB();
+}
+BLYNK_WRITE(V6)  {
+  manualBlynkRGB[1] = param.asInt();
+  setManualRGB();
+}
+BLYNK_WRITE(V7)  {
+  manualBlynkRGB[2] = param.asInt();
+  setManualRGB();
+}
+
+
+void setManualRGB() {
+  if ( runByTime )
+    return;
+
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    leds[i].setRGB(manualBlynkRGB[0], manualBlynkRGB[1], manualBlynkRGB[2]);
+  }
+  
+  if (manualBlynkRGB[0] + manualBlynkRGB[1] + manualBlynkRGB[2] == 0)
+    effectStopProtocol();
 }
--- effect_control.ino
+++ effect_control.ino
@@ -6,19 +6,25 @@ bool firtTimeEffect[EFFECT_COUNTS];
 void effect_control_setup() {
 
   allEffects[0].create(continuousUse);  /// DemoReel
-  allEffects[0].addWorkTime(17, 52, 17, 52);
+  allEffects[0].addWorkTime(21, 20, 21, 54);
 
-  allEffects[1].create(singleUse);  /// Avize Kemeri
-  allEffects[1].addWorkTime(15, 00, 20, 00);
+  allEffects[1].create(singleUse);  /// Circle night lamb
+  allEffects[1].addWorkTime(15, 00, 21, 44);
+
+  allEffects[2].create(continuousUse);  /// Flashing Stars
+  allEffects[2].addWorkTime(21, 34, 21, 43);
+
+  allEffects[3].create(continuousUse);  /// Dynamic Circle
+  allEffects[3].addWorkTime(21, 34, 02, 54);
 
 }
 void effect_control_loop() {
 
   timeClient.update();
 
-  for ( byte i = 0 ; i < effectCount; i++ ) {
+  for ( byte i = 0 ; i < EFFECT_COUNTS; i++ ) {
     if (allEffects[i].enable) {
-      bool runTime = allEffects[i].isTime();
+      bool runTime = runByTime  ? allEffects[i].isTime()  : allEffects[i].manualWork;
       getEffectOnOFF(i, runTime);
       if ( runTime && allEffects[i].useType == continuousUse ) {
         runEffect(i, continuousUse);
@@ -38,6 +44,10 @@ void runEffect(byte num, bool useType) {
     switch ( allEffects[num].id ) {
       case 0 : ReelEfect();
         break;
+      case 2 : flashingStars();
+        break;
+      case 3 : dynamic_circle_loop ();
+        break;
     }
   }
 }
@@ -49,16 +59,24 @@ void getEffectOnOFF(byte i, bool runTime) {
     if ( firtTimeEffect[i] )
       effectStartProtocol(i);
     else
-      effectStopProtocol(i);
+      effectStopProtocol();
 
   }
 }
 void effectStartProtocol(byte num) {
   runEffect(num, allEffects[num].useType);
 }
-void effectStopProtocol(byte num) {
+void effectStopProtocol() {
   clearLeds();
-  for ( byte i = 0 ; i < EFFECT_COUNTS ; i++ )
-    if ( allEffects[i].id != -1 && allEffects[i].isTime()&& allEffects[i].useType == singleUse )
+  for ( byte i = 0 ; i < EFFECT_COUNTS ; i++ ) {
+    if ( allEffects[i].id != -1 && (runByTime  ? allEffects[i].isTime()  : allEffects[i].manualWork) && allEffects[i].useType == singleUse )
       runEffect(i, singleUse);
+  }
+}
+
+void changeEffectManualWorkStatus(byte num, bool manualWorkStatus) {
+  allEffects[num].manualWork = manualWorkStatus;
+}
+bool getEffectManualWorkStatus(byte num) {
+  return allEffects[num].manualWork;
 }
