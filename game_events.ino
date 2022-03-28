d5c23a51d869e076dd0b963c65e20950e3355dac
fthgncl <gencal.fatih61@gmail.com>
2022-03-28 07:46:53 +0300

    valorant için test efekti eklendi

--- game_events.ino
+++ game_events.ino
@@ -9,21 +9,28 @@ void getEvent(int gameID , int effectNo ) {
   gameEffectStopedProtocol();
 }
 
-void valorantEffect(int effectNo){
-  gameEffectStartProtocol();
-  
-  leds[100].setRGB(255,0,0);
-  FastLED.show();
-  
-  delay(1000);
-  
-  gameEffectStopedProtocol();
-}
-
-void gameEffectStartProtocol(){
+void gameEffectStartProtocol() {
   memcpy(ledsCopy, leds, sizeof(leds));
   FastLED.clear();
 }
-void gameEffectStopedProtocol(){
+void gameEffectStopedProtocol() {
   memcpy(leds, ledsCopy, sizeof(leds));
+  blynkUpdateDashBoard();
+}
+
+
+void valorantEffect(int effectNo) {
+
+  switch (effectNo) {
+    case 1: {
+        for ( int i = 0 ; i < 100 ; i++) {
+          addGlitter(80);
+          FastLED.show();
+          delay(10);
+        }
+      }
+      break;
+  }
+
+
 }
