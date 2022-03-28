c3a790ff827ba4ed39dbb0d66b6677a5abcc8d01
fthgncl <gencal.fatih61@gmail.com>
2022-03-28 07:07:48 +0300

    Oyun efektleri için başlangıç ve bitiş protokolleri eklendi

--- game_events.ino
+++ game_events.ino
@@ -1,4 +1,20 @@
-void getEvent(int gameID , int effectNo ){
-  Serial.println(gameID);
-  Serial.println(effectNo);
+CRGB ledsCopy[NUM_LEDS];
+
+void valorantEffect(int effectNo){
+  gameEffectStarted();
+  
+  leds[100].setRGB(255,0,0);
+  FastLED.show();
+  
+  delay(1000);
+  
+  gameEffectStoped();
+}
+
+void gameEffectStartProtocol(){
+  memcpy(ledsCopy, leds, sizeof(leds));
+  FastLED.clear();
+}
+void gameEffectStopedProtocol(){
+  memcpy(leds, ledsCopy, sizeof(leds));
 }
