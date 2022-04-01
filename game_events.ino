975bcca258f9d426c6995cf4ce8a19b646454b05
fthgncl <gencal.fatih61@gmail.com>
2022-04-01 07:35:50 +0300

    sadeleştirildi

--- game_events.ino
+++ game_events.ino
@@ -14,55 +14,8 @@ void gameEffectStartProtocol() {
   memcpy(ledsCopy, leds, sizeof(leds));
   FastLED.clear();
 }
+
 void gameEffectStopedProtocol() {
   memcpy(leds, ledsCopy, sizeof(leds));
   blynkUpdateDashBoard();
-}
-
-
-void valorantEffect(int effectNo) {
-
-  switch (effectNo) {
-    case 1: deathEffect();
-      break;
-  }
-
-
-}
-void deathEffect() {
-  changeAllLedsClorRed(0);
-  delay(20);
-  changeAllLedsClorRed(255);
-  delay(20);
-  changeAllLedsClorRed(0);
-  delay(20);
-  changeAllLedsClorRed(255);
-  delay(20);
-  changeAllLedsClorRed(0);
-  delay(20);
-  changeAllLedsClorRed(255);
-  delay(20);
-  changeAllLedsClorRed(0);
-  delay(20);
-  changeAllLedsClorRed(255);
-  delay(20);
-  changeAllLedsClorRed(0);
-  delay(20);
-  changeAllLedsClorRed(255);
-  delay(20);
-  changeAllLedsClorRed(0);
-  delay(20);
-  changeAllLedsClorRed(255);
-
-  
-  while ( leds[0].r > 5 ) {
-    changeAllLedsClorRed(leds[0].r-5);
-  }
-}
-void changeAllLedsClorRed(byte _red) {
-  for ( int i = 0 ; i < NUM_LEDS ; i++ ) {
-    leds[i].setRGB(_red, 0, 0);
-  }
-  
-  FastLED.show();
 }
