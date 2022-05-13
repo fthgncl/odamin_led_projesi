e866ad10320ac70ba32c6a5290bcab6f443b2ac7
fthgncl <gencal.fatih61@gmail.com>
2022-05-13 05:47:03 +0300

    ses algılayıcısı kaldırıldı

--- odamin_led_projesi.ino
+++ odamin_led_projesi.ino
@@ -4,17 +4,15 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
   
+  effect_control_setup();
   blynk_setup();
   fastled_setup();
   startanimation();
-  effect_control_setup();
-  sound_sensor_setup();
   web_server_setup();
 }
 void loop() {
   
   blynk_loop();
-  sound_sensor_loop();
   effect_control_loop();
   FastLED.show();
   
