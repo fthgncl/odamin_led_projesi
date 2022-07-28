ea2eca85b09b1b55dd783017d63d4b766f1dcf23
fthgncl <gencal.fatih61@gmail.com>
2022-07-28 11:40:25 +0300

    Update effect_control.ino
    optimize edildi

--- effect_control.ino
+++ effect_control.ino
@@ -1,21 +1,22 @@
-
+unsigned long lastMoment = 0;
+unsigned long nowMoment = 0;
 
 bool firtTimeEffect[MAX_EFFECT_COUNTS];
 
 void effect_control_setup() {
 
-  CreateEffect(continuousUse, ReelEfect_setup, ReelEfect, V1,19, 00, 21, 00); /// DemoReel
-  CreateEffect(singleUse, avizeKemeri_setup, avizeKemeri_loop, V2,04, 30, 05, 10); /// Circle night lamb
-  CreateEffect(continuousUse, flashingStars_setup, flashingStars_loop, V3,23, 00, 00, 00); /// Flashing Stars
-  CreateEffect(continuousUse, dynamic_circle_setup, dynamic_circle_loop, V8,23, 00, 23, 30); /// Dynamic Circle
-  CreateEffect(continuousUse, wandering_spots_setup, wandering_spots_loop, V10,16, 00, 19, 00); /// Wandering Spots
-  CreateEffect(continuousUse, partly_cloudy_setup, partly_cloudy_loop, V12,21, 00, 23, 00); /// Partly Cloudy
-  CreateEffect(continuousUse, random_moving_light_setup, random_moving_light_loop, V13,23, 30, 00, 00); /// Random Moving Light
+  CreateEffect(continuousUse, ReelEfect_setup, ReelEfect, V1, 19, 00, 21, 00); /// DemoReel
+  CreateEffect(singleUse, avizeKemeri_setup, avizeKemeri_loop, V2, 04, 30, 05, 10); /// Circle night lamb
+  CreateEffect(continuousUse, flashingStars_setup, flashingStars_loop, V3, 23, 00, 00, 00); /// Flashing Stars
+  CreateEffect(continuousUse, dynamic_circle_setup, dynamic_circle_loop, V8, 23, 00, 23, 30); /// Dynamic Circle
+  CreateEffect(continuousUse, wandering_spots_setup, wandering_spots_loop, V10, 16, 00, 19, 00); /// Wandering Spots
+  CreateEffect(continuousUse, partly_cloudy_setup, partly_cloudy_loop, V12, 21, 00, 23, 00); /// Partly Cloudy
+  //CreateEffect(continuousUse, random_moving_light_setup, random_moving_light_loop, V13, 23, 30, 00, 00); /// Random Moving Light
 
 }
 void effect_control_loop() {
 
-  timeClient.update();
+  updateTime();
   bool runTime;
   for ( byte i = 0 ; i < MAX_EFFECT_COUNTS; i++ ) {
     if (allEffects[i].enable) {
@@ -25,6 +26,13 @@ void effect_control_loop() {
         allEffects[i].loopFunction();
       }
     }
+  }
+}
+void updateTime() {
+  nowMoment = millis();
+  if ( nowMoment - lastMoment > 10000 ) {
+    lastMoment = nowMoment;
+    timeClient.update();
   }
 }
 void getEffectOnOFF(byte i, bool runTime) {
