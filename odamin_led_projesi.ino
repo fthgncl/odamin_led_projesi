4e9c2d79ff679de35fd8f0a4cfeec743f627e495
fthgncl <gencal.fatih61@gmail.com>
2022-08-05 23:23:50 +0300

    işlem sırası düzenlendi

--- odamin_led_projesi.ino
+++ odamin_led_projesi.ino
@@ -3,22 +3,22 @@
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  
-  effect_control_setup();
+
   blynk_setup();
+  prayerTimesUpdate();
+  effect_control_setup();
   fastled_setup();
   startanimation();
   web_server_setup();
-  prayerTimesUpdate();
 }
 void loop() {
-  
+
   blynk_loop();
   effect_control_loop();
   FastLED.show();
 
-  EVERY_N_MILLISECONDS(50){
+  EVERY_N_MILLISECONDS(50) {
     button_control();
   }
-  
+
 }
