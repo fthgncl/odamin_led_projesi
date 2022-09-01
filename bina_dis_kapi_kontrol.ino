6be1bac68c7e4a8a297d1f36f8fe055d051d8c4d
fthgncl <gencal.fatih61@gmail.com>
2022-09-01 16:37:23 +0300

    dış kapı http isteği yeniden oluşturuldu

--- bina_dis_kapi_kontrol.ino
+++ bina_dis_kapi_kontrol.ino
@@ -1,5 +1,27 @@
 void binaKapisiniAc() {
+  Serial.println(httpGETRequest("http://192.168.1.199/kapiyiac"));
+}
+String httpGETRequest(const char* serverName) {
   WiFiClient client;
   HTTPClient http;
-  http.begin(client, "192.168.1.199/kapiyiac");
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
 }
