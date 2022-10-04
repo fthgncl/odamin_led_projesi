f6da3c20218252d5cc26e489fa81405596092d6c
fthgncl <gencal.fatih61@gmail.com>
2022-10-04 10:07:53 +0300

    efekt güncellendi

--- EFFECT_circle_night_lamp.ino
+++ EFFECT_circle_night_lamp.ino
@@ -3,7 +3,9 @@ void avizeKemeri_loop() {
   
 }
 void avizeKemeri_setup(byte effectID) {
-  changeEffectTimes(effectID,namazVakti[IMSAK][SAAT], namazVakti[IMSAK][DAKIKA], namazVakti[GUNES][SAAT], namazVakti[GUNES][DAKIKA]);
+  if ( allEffects[effectID].manualWork == false )
+    changeEffectTimes(effectID,namazVakti[IMSAK][SAAT], namazVakti[IMSAK][DAKIKA], namazVakti[GUNES][SAAT], namazVakti[GUNES][DAKIKA]-1);
+  
   byte color[3];
   color[0] = random(50);
   color[1] = random(50);
