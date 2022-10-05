e815ac2b7f9766c22530b517cbeb7399b54dd758
fthgncl <gencal.fatih61@gmail.com>
2022-10-05 04:22:52 +0300

    efekt güncellendi

--- EFFECT_wandering_spots.ino
+++ EFFECT_wandering_spots.ino
@@ -5,7 +5,7 @@ void wandering_spots_loop() {
   fadeToBlackBy( leds, NUM_LEDS, 10);
   uint8_t dothue = 0;
   for ( int i = 0; i < 3; i++) {
-    leds[beatsin16( i + 7, 0, NUM_LEDS - 1 )] |= CHSV(dothue, 200, 255);
+    leds[beatsin16( i + 1, 0, NUM_LEDS - 1 )] |= CHSV(dothue, 200, 255);
     dothue += 85;
   }
 }
